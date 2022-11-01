3abc8516ba4a3068f580ad2de496661e34ea5dc6
Nikita Levchuk <code@nikitalevchuk.me>
2022-11-01 12:06:17 +0100

    feature: add configuration DB per environment

--- src/data-source.config.ts
+++ src/data-source.config.ts
@@ -2,13 +2,26 @@ import { DataSourceOptions } from 'typeorm';
 import { Deck } from './decks/deck.entity';
 import { Card } from './cards/card.entity';
 
-export default (): DataSourceOptions => ({
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  migrations: [`${__dirname}/migrations/*.ts`],
-  entities: [Deck, Card],
-});
+export default (): DataSourceOptions => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+    case 'production':
+      return {
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        migrations: [`${__dirname}/migrations/*.ts`],
+        entities: [Deck, Card],
+      }
+    default:
+      return {
+        type: 'sqlite',
+        database: ':memory:',
+        migrations: [`${__dirname}/migrations/*.ts`],
+        entities: [Deck, Card],
+      };
+  }
+};
